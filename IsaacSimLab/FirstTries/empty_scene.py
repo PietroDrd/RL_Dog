7bc396ef91b19977b36440f668d270cae3889c1c
PietroDrd <dardanopietro@libero.it>
2024-06-21 15:56:14 +0200

    done first examples locally, with GUI
    WORKS

---
+++ IsaacSimLab/FirstTries/empty_scene.py
@@ -0,0 +1,104 @@
+
+# do -->   conda activate isaaclabpip
+#          ctr+shift+p --> select interpreter 
+
+import argparse
+from omni.isaac.lab.app import AppLauncher
+
+parser = argparse.ArgumentParser(description="Creating empty stage.")
+AppLauncher.add_app_launcher_args(parser)
+args_cli = parser.parse_args()
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+# SCENES IMPORTS
+
+from omni.isaac.lab.sim             import SimulationCfg, SimulationContext
+
+import omni.isaac.lab.sim           as sim_utils
+import omni.isaac.lab.utils.math    as math_utils
+import omni.isaac.core.utils.prims  as prim_utils
+
+from omni.isaac.lab.assets          import RigidObject, RigidObjectCfg
+from omni.isaac.lab.utils.assets    import ISAAC_NUCLEUS_DIR
+
+# EXTRA ISAACSIM
+
+import torch
+
+def design_scene():
+    """Designs the scene by spawning ground plane, light, objects and meshes from usd files."""
+    # Ground-plane
+    cfg_ground = sim_utils.GroundPlaneCfg()
+    cfg_ground.func("/World/defaultGroundPlane", cfg_ground)
+
+    # Lights
+    cfg = sim_utils.DomeLightCfg(intensity=3000.0, color=(0.8, 0.8, 0.8))
+    cfg.func("/World/Light", cfg)
+
+    # # spawn distant light
+    # cfg_light_distant = sim_utils.DistantLightCfg(
+    #     intensity=3000.0,
+    #     color=(0.75, 0.75, 0.75),
+    # )
+    # cfg_light_distant.func("/World/lightDistant", cfg_light_distant, translation=(1, 0, 10))
+
+    # create a new xform prim for all objects to be spawned under
+    prim_utils.create_prim("/World/Objects", "Xform")
+    # spawn a red cone
+    cfg_cone = sim_utils.ConeCfg(
+        radius=0.15,
+        height=0.5,
+        visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(1.0, 0.0, 0.0)),
+    )
+    cfg_cone.func("/World/Objects/Cone1", cfg_cone, translation=(-1.0, 1.0, 1.0))
+    cfg_cone.func("/World/Objects/Cone2", cfg_cone, translation=(-1.0, -1.0, 1.0))
+
+    # spawn a green cone with colliders and rigid body
+    cfg_cone_rigid = sim_utils.ConeCfg(
+        radius=0.15,
+        height=0.5,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(),
+        mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
+        collision_props=sim_utils.CollisionPropertiesCfg(),
+        visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 1.0, 0.0)),
+    )
+    cfg_cone_rigid.func(
+        "/World/Objects/ConeRigid", cfg_cone_rigid, translation=(0.0, 0.0, 2.0), orientation=(0.5, 0.0, 0.5, 0.0)
+    )
+
+    # spawn a usd file of a table into the scene
+    cfg = sim_utils.UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/SeattleLabTable/table_instanceable.usd")
+    cfg.func("/World/Objects/Table", cfg, translation=(0.0, 0.0, 1.05))
+
+
+####################################################################
+"""
+                    COMMAND TO RUN THE SCRIPT 
+cd 
+cd IsaacLab_
+./isaaclab.sh -p ~/RL_Dog/IsaacSimLab/FirstTries/empty_scene.py
+"""
+####################################################################
+
+def main():
+    sim_cfg = SimulationCfg(dt=0.01, substeps=1)
+    sim = SimulationContext(sim_cfg)
+    # Set main camera
+    sim.set_camera_view([2.5, 2.5, 2.5], [0.0, 0.0, 0.0])
+
+    design_scene()
+
+    # Play the simulator
+    sim.reset()
+    print("[INFO]: Setup completed ")
+    while simulation_app.is_running():
+        # perform step
+        sim.step()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
---
+++ IsaacSimLab/FirstTries/rigid_groups.py
@@ -0,0 +1,138 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+This script demonstrates how to create a rigid object and interact with it.
+
+.. code-block:: bash
+    cd
+    cd IsaacLab_
+    ./isaaclab.sh -p ~/RL_Dog/IsaacSimLab/FirstTries/rigid_groups.py 
+"""
+
+"""Launch Isaac Sim Simulator first."""
+
+
+import argparse
+
+from omni.isaac.lab.app import AppLauncher
+
+parser = argparse.ArgumentParser(description="Tutorial on spawning and interacting with a rigid object.")
+AppLauncher.add_app_launcher_args(parser)
+args_cli = parser.parse_args()
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import torch
+
+import omni.isaac.core.utils.prims as prim_utils
+
+import omni.isaac.lab.sim as sim_utils
+import omni.isaac.lab.utils.math as math_utils
+from omni.isaac.lab.assets import RigidObject, RigidObjectCfg
+from omni.isaac.lab.sim import SimulationContext
+
+
+def design_scene():
+    """Designs the scene."""
+    # Ground-plane
+    cfg = sim_utils.GroundPlaneCfg()
+    cfg.func("/World/defaultGroundPlane", cfg)
+    # Lights
+    cfg = sim_utils.DomeLightCfg(intensity=3000.0, color=(0.8, 0.8, 0.8))
+    cfg.func("/World/Light", cfg)
+
+    # Create separate groups called "Origin1", "Origin2", "Origin3"
+    # Each group will have a robot in it
+    origins = [[0.25, 0.25, 0.0], [-0.25, 0.25, 0.0], [0.25, -0.25, 0.0], [-0.25, -0.25, 0.0]]
+    for i, origin in enumerate(origins):
+        prim_utils.create_prim(f"/World/Origin{i}", "Xform", translation=origin)
+
+    # Rigid Object
+    cone_cfg = RigidObjectCfg(
+        prim_path="/World/Origin.*/Cone",
+        spawn=sim_utils.ConeCfg(
+            radius=0.1,
+            height=0.2,
+            rigid_props=sim_utils.RigidBodyPropertiesCfg(),
+            mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
+            collision_props=sim_utils.CollisionPropertiesCfg(),
+            visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 1.0, 0.0), metallic=0.2),
+        ),
+        init_state=RigidObjectCfg.InitialStateCfg(),
+    )
+    cone_object = RigidObject(cfg=cone_cfg)
+
+    # return the scene information
+    scene_entities = {"cone": cone_object}
+    return scene_entities, origins
+
+
+def run_simulator(sim: sim_utils.SimulationContext, entities: dict[str, RigidObject], origins: torch.Tensor):
+    """Runs the simulation loop."""
+    # Extract scene entities
+    cone_object = entities["cone"]
+
+    sim_dt = sim.get_physics_dt()
+    sim_time = 0.0
+    count = 0
+
+    # Simulate physics
+    while simulation_app.is_running():
+        # reset
+        if count % 250 == 0:
+            # reset counters
+            sim_time = 0.0
+            count = 0
+            # reset root state
+            root_state = cone_object.data.default_root_state.clone()
+            # sample a random position on a cylinder around the origins
+            root_state[:, :3] += origins
+            root_state[:, :3] += math_utils.sample_cylinder(
+                radius=0.1, h_range=(0.25, 0.5), size=cone_object.num_instances, device=cone_object.device
+            )
+            # write root state to simulation
+            cone_object.write_root_state_to_sim(root_state)
+            # reset buffers
+            cone_object.reset()
+            print("----------------------------------------")
+            print("[INFO]: Resetting object state...")
+        # apply sim data
+        cone_object.write_data_to_sim()
+
+        sim.step()
+        sim_time += sim_dt
+        count += 1
+
+        cone_object.update(sim_dt)
+        # print the root position
+        if count % 50 == 0:
+            print(f"Root position (in world): {cone_object.data.root_state_w[:, :3]}")
+
+
+def main():
+    """Main function."""
+    # Load kit helper
+    sim_cfg = sim_utils.SimulationCfg()
+    sim = SimulationContext(sim_cfg)
+
+    sim.set_camera_view(eye=[1.5, 0.0, 1.0], target=[0.0, 0.0, 0.0])
+
+    scene_entities, scene_origins = design_scene()
+    scene_origins = torch.tensor(scene_origins, device=sim.device)
+
+    sim.reset()
+    print("[INFO]: Setup complete...")
+
+    run_simulator(sim, scene_entities, scene_origins)
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
