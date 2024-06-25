41c618cb61ea6f26be0fec69509b2a555ea3f9a8
PietroDrd <dardanopietro@libero.it>
2024-06-25 16:36:56 +0200

    tutorials + assets + folders

---
+++ assets/aliengo_const.py
@@ -0,0 +1,20 @@
+class UnitreeLeggedAliengo:
+    HIP_MAX = 1.047     # unit: radian ( = 60   degree)
+    HIP_MIN = -0.873    # unit: radian ( = -50  degree)
+    THIGH_MAX = 3.927   # unit: radian ( = 225  degree)
+    THIGH_MIN = -0.524  # unit: radian ( = -30  degree)
+    CALF_MAX = -0.611   # unit: radian ( = -35  degree)
+    CALF_MIN = -2.775   # unit: radian ( = -159 degree)
+
+""" Usage example:
+    
+    aliengo = UnitreeLeggedAliengo()
+
+"""
+
+# print(f"Hip max:   {aliengo.HIP_MAX} rad")
+# print(f"Hip min:   {aliengo.HIP_MIN} rad")
+# print(f"Thigh max: {aliengo.THIGH_MAX} rad")
+# print(f"Thigh min: {aliengo.THIGH_MIN} rad")
+# print(f"Calf max:  {aliengo.CALF_MAX} rad")
+# print(f"Calf min:  {aliengo.CALF_MIN} rad")
