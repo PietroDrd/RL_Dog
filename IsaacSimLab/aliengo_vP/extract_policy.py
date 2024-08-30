212110b344a55044e584884d59faacad152e7321
PietroDrd <dardanopietro@libero.it>
2024-08-30 11:03:46 +0200

    nn

--- IsaacSimLab/aliengo_vP/extract_policy.py
+++ IsaacSimLab/aliengo_vP/extract_policy.py
@@ -1,7 +1,7 @@
 import torch
 import os
 
-path = "/home/rluser/RL_Dog/runs/AlienGo_vP_stoptry_22_08_FULL_STATE/checkpoints/agent_21000.pt"
+path = "/home/rluser/RL_Dog/runs/AlienGo_vP_stoptry_29_08_FULL_STATE_v2/checkpoints/best_agent.pt"
 policy_nn = torch.load(path)['policy']
-torch.save(policy_nn, "/home/rluser/RL_Dog/models/policy_nn.pt")
+torch.save(policy_nn, "/home/rluser/RL_Dog/models/FULL_STATE__NN_v2.pt")
 
---
+++ models/README.md
@@ -0,0 +1,7 @@
+### v1
+- scale factor: 0.8
+- Delta Joint position
+
+### v2
+- scale factor: 1.0  --> Doesen't Affect
+- Delta Joint position
