f17e1e10cc41e96229ab99ba10a65d88d53fa6a8
PietroDrd <dardanopietro@libero.it>
2024-09-10 11:08:40 +0200

    aliengo confing

--- omni/isaac/lab_assets/unitree.py
+++ omni/isaac/lab_assets/unitree.py
@@ -50,6 +50,106 @@ This model is taken from: https://github.com/Improbable-AI/walk-these-ways
 ##
 
 
+#################################### ALIENGO ########################################
+
+"""
+		joint_pos={
+            ".*L_hip_joint": 0.1,
+            ".*R_hip_joint": -0.1,
+            "F[L,R]_thigh_joint": 0.8,
+            "R[L,R]_thigh_joint": 1.0,
+            ".*_calf_joint": -1.5,
+        },
+"""
+
+AliengoCFG_Black = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path="/home/rl_sim/IsaacLab_/source/extensions/omni.isaac.lab_assets/data/Robots/Unitree/aliengo_black.usd", # in future modify it with Nucleus directory 
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.42),
+		joint_pos={
+            ".*L_hip_joint": 0.1,
+            ".*R_hip_joint": -0.1,
+            "F[L,R]_thigh_joint": 0.8,
+            "R[L,R]_thigh_joint": 1.0,
+            ".*_calf_joint": -1.5,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    actuators={
+        "base_legs": DCMotorCfg(
+            joint_names_expr=[".*_hip_joint", ".*_thigh_joint", ".*_calf_joint"],
+            effort_limit=35.5,	    # original = 33.5
+            saturation_effort=35.5, # original = 33.5
+            velocity_limit=28.0,    # original = 21.0
+            stiffness=25.0, #kp
+            damping=0.5,	#kd
+            friction=0.0,
+        ),
+    },
+    # prim_path="{ENV_REGEX_NS}/Robot"
+)
+
+AliengoCFG_Color = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path="/home/rl_sim/IsaacLab_/source/extensions/omni.isaac.lab_assets/data/Robots/Unitree/aliengo_color.usd", # in future modify it with Nucleus directory 
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.42),
+    	joint_pos={
+            ".*L_hip_joint": 0.0,
+            ".*R_hip_joint": 0.0,
+            "F[L,R]_thigh_joint": 0.0,
+            "R[L,R]_thigh_joint": 0.0,
+            ".*_calf_joint": 0.0,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    actuators={
+        "base_legs": DCMotorCfg(
+            joint_names_expr=[".*_hip_joint", ".*_thigh_joint", ".*_calf_joint"],
+            effort_limit=35.5,      # original = 33.5
+            saturation_effort=35.5, # original = 33.5
+            velocity_limit=28.0,    # original = 21.0
+            stiffness=25.0,
+            damping=0.5,
+            friction=0.0,
+        ),
+    },
+    # prim_path="{ENV_REGEX_NS}/Robot"
+)
+
+#################################### ALIENGO ########################################
+
 UNITREE_A1_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
         usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/Unitree/A1/a1.usd",
